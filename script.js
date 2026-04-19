978e010ca6886da3620c5199fd6f4031dc51e6c4
Rebecca Kunce <rebeccakunce@gmail.com>
2026-04-19 18:30:40 -0500

    API Explorer Practice BK

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// so much empty
+// so much empty so much sad
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+       .then(function (response) {
+           // Convert the response to JSON
+           return response.json();
+      })
+       .then(function (json) {
+           console.log(json); // Log the JSON data
+       })
+       .catch(function (error) {
+           console.error("Our Apologies There was an error fetching your data:", error);
+       });
+
+
+
+const container = document.title.getElementById("postList");
+
+postList.innerHTML = `
+                <h2>${item.title}</h2>
+                <p>${item.body}</p>
+            `;
+            
+            // Append the new div to the main container
+            container.appendChild(itemDiv);
